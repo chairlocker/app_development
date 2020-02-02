1ad1e3715b043f941e925ba689b56906714a9677
imcyano <musimono0610@gmail.com>
2020-02-02 15:54:40 +0900

    Initial Commit

---
+++ chairlocker_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chairlocker_app
+//
+//  Created by Akari Shimono on 2020/02/02.
+//  Copyright © 2020 kogumffi01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
